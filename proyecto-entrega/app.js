f85966c674fa1e63fd05302c9d50aae0b7ef3839
Thiago Borro <borrothiago@gmail.com>
2024-06-21 14:13:23 -0300

    modified app.js

--- proyecto-entrega/app.js
+++ proyecto-entrega/app.js
@@ -20,6 +20,8 @@ var nikeRouter = require('./routes/nike');
 var registroRouter = require('./routes/registro');
 
 var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -28,9 +30,25 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 app.use('/indumentaria', indumentariaRouter);
 app.use('/nike', nikeRouter);
 app.use('/registro', registroRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
 
 module.exports = app;
