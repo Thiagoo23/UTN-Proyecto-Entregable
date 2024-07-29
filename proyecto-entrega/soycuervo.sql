f4060c2196cd4671c7b3a17add86550dfeb36e8c
Thiago Borro <borrothiago@gmail.com>
2024-07-29 20:30:54 -0300

    sql

--- proyecto-entrega/soycuervo.sql
+++ proyecto-entrega/soycuervo.sql
@@ -1,6 +1,26 @@
 ﻿# Host: localhost  (Version 5.5.5-10.4.28-MariaDB)
-# Date: 2024-07-25 20:56:39
+# Date: 2024-07-29 20:30:29
 # Generator: MySQL-Front 6.1  (Build 1.26)
+
+
+#
+# Structure for table "noticias"
+#
+
+DROP TABLE IF EXISTS `noticias`;
+CREATE TABLE `noticias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(250) NOT NULL,
+  `subtitulo` text NOT NULL,
+  `fecha_publicacion` date NOT NULL,
+  `cuerpo` text NOT NULL,
+  `imagen` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+#
+# Data for table "noticias"
+#
 
 
 #
@@ -9,13 +29,13 @@
 
 DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE `usuarios` (
-  `id_emp` int(255) NOT NULL AUTO_INCREMENT,
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `usuario` varchar(250) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(250) NOT NULL,
   `telefono` int(20) NOT NULL,
   `n_socio` int(15) NOT NULL,
-  PRIMARY KEY (`id_emp`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 #
