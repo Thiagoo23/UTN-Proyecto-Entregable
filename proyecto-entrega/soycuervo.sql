e436037dbc0d93f41347875166ae490c6324cd19
Thiago Borro <borrothiago@gmail.com>
2024-08-01 18:42:34 -0300

    sql

--- proyecto-entrega/soycuervo.sql
+++ proyecto-entrega/soycuervo.sql
@@ -1,45 +1,91 @@
-﻿# Host: localhost  (Version 5.5.5-10.4.28-MariaDB)
-# Date: 2024-07-29 20:30:29
-# Generator: MySQL-Front 6.1  (Build 1.26)
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 01-08-2024 a las 23:42:03
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-#
-# Structure for table "noticias"
-#
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-DROP TABLE IF EXISTS `noticias`;
-CREATE TABLE `noticias` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `titulo` varchar(250) NOT NULL,
-  `subtitulo` text NOT NULL,
-  `fecha_publicacion` date NOT NULL,
-  `cuerpo` text NOT NULL,
-  `imagen` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+--
+-- Base de datos: `soycuervo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cuenta`
+--
+
+CREATE TABLE `cuenta` (
+  `id` int(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-#
-# Data for table "noticias"
-#
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
 
-#
-# Structure for table "usuarios"
-#
-
-DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE `usuarios` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `id` int(255) NOT NULL,
   `usuario` varchar(250) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(250) NOT NULL,
   `telefono` int(20) NOT NULL,
-  `n_socio` int(15) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `n_socio` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-#
-# Data for table "usuarios"
-#
+--
+-- Volcado de datos para la tabla `usuarios`
+--
 
-INSERT INTO `usuarios` VALUES (12,'thiagoborro','borrothiago@gmail.com','21f6f6a3238819e436724fe2a508ecde',12345678,13411);
+INSERT INTO `usuarios` (`id`, `usuario`, `email`, `password`, `telefono`, `n_socio`) VALUES
+(1, 'thiagoborro', 'borrothiago@gmail.com', 'ec70276cc4404ff6db741f5414caab77', 123456789, 13411);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cuenta`
+--
+ALTER TABLE `cuenta`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cuenta`
+--
+ALTER TABLE `cuenta`
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
